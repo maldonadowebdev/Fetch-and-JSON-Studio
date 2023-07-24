709bdd316320f8fe60bda843edcfc7b4a59d490d
maldonadowebdev <maldonadowebdev@gmail.com>
2023-07-24 16:29:03 -0400

    Made a few minor corrections to layout

--- script.js
+++ script.js
@@ -2,24 +2,27 @@ window.addEventListener("load", function() {
 
 fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
     response.json().then( function(json) {
-        console.log(json);
-        let cont = document.getElementById("container");
+        let containerDiv = document.getElementById("container");
         let output = [];
         for (let i = 0; i < json.length; i++){
             let add = `
             <div class="astronaut">
-            <h3>${json[i].firstName} ${json[i].lastName}</h3>
-            <ul>
-            <li>ID: ${json[i].id}</li>
-            <li>Hours in Space: ${json[i].hoursInSpace}</li>
-            <li>Active: ${json[i].active}</li>
-            <li>Skills: ${json[i].skills}</li>
-            </ul>
-            <img class="avatar" src="${json[i].picture}">
-            </div>`;
+                <span>
+                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                    <ul>         
+                        <li>ID: ${json[i].id}</li>
+                        <li>Hours in Space: ${json[i].hoursInSpace}</li>
+                        <li>Active: ${json[i].active}</li>
+                        <li>Skills: ${json[i].skills}</li>
+                    </ul>
+                </span>
+                <img class="avatar" src="${json[i].picture}">
+            </div>
+            `;
             output.push(add);
+
         };
-        cont.innerHTML = output.join("");
+        containerDiv.innerHTML = output.join("");
         });
     });
 });
