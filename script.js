c6a726fb9d62e739ace371cf1be813fd6ebb86c0
maldonadowebdev <maldonadowebdev@gmail.com>
2023-07-25 14:25:31 -0400

    Added the Green section of Bonus Misson

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 window.addEventListener("load", function() {
 
 fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+
     response.json().then( function(json) {
+
         let containerDiv = document.getElementById("container");
         let output = [];
         for (let i = 0; i < json.length; i++){
+           
+            let color;
+            if (json[i].active === true){
+                color = 'style="color:green"';
+            };
+
             let add = `
             <div class="astronaut">
                 <span>
                     <h3>${json[i].firstName} ${json[i].lastName}</h3>
                     <ul>         
-                        <li>ID: ${json[i].id}</li>
+                        
                         <li>Hours in Space: ${json[i].hoursInSpace}</li>
-                        <li>Active: ${json[i].active}</li>
-                        <li>Skills: ${json[i].skills}</li>
+                        <li ${color}>Active: ${json[i].active}</li>
+                        <li>Skills: ${json[i].skills.join(", ")}</li>
                     </ul>
                 </span>
                 <img class="avatar" src="${json[i].picture}">
             </div>
             `;
+            
             output.push(add);
 
         };
